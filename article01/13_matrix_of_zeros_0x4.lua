ef42ef3f11b35bb8f6ca3df8fbb6141268362d21
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Matrix 0x4 of zeros

---
+++ article01/13_matrix_of_zeros_0x4.lua
@@ -0,0 +1,22 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 13:
+-- Vytvoření matice 0x4 prvků
+--
+
+alg = require "sci.alg"
+
+-- alokace matice: žádné řádky, (teoreticky) 4 sloupce
+mat = alg.mat(0, 4)
+
+-- počet prvků
+print(#mat)
+
+print "Matrix value\n"
+print(mat)
+
+-- finito
