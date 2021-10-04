e905dd7cb8e8f6e46b356048179c2bdec8a6631d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Matrix 3x4 of zeros

---
+++ article01/12_matrix_of_zeros_3x4.lua
@@ -0,0 +1,19 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 12:
+-- Vytvoření matice 3x4 prvků
+--
+
+alg = require "sci.alg"
+
+-- alokace matice: 3 řádky, 4 sloupce
+mat = alg.mat(3, 4)
+
+print "Matrix value\n"
+print(mat)
+
+-- finito
