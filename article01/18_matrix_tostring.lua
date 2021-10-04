1ac6b2e5dc4cee97390aab06d7c6c17bd83d323e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Conversion from matrix to string

---
+++ article01/18_matrix_tostring.lua
@@ -0,0 +1,26 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 18:
+-- Konverze matice na řetězec
+--
+
+alg = require "sci.alg"
+
+-- běžná tabulka jazyka Lua
+tbl = {{1,  2,  3,  4},
+       {5,  6,  7,  8},
+       {9, 10, 11, 12}}
+
+-- konverze tabulky na matici
+mat = alg.tomat(tbl)
+
+-- konverze na řetězec
+s = tostring(mat)
+print "Matrix as string\n"
+print(s)
+
+-- finito
