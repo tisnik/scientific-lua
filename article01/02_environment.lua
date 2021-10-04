0dc218a3a8ff3dfd14ac6ca3692f617dca7e54f0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Is environment changed by bare require?

---
+++ article01/02_environment.lua
@@ -0,0 +1,25 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 2:
+-- Výpis globálních symbolů před i po načtení knihovny "sci.alg"
+--
+
+print "\nBefore 'require'\n"
+
+for k, v in pairs(_G) do
+    print(k, v)
+end
+
+require "sci.alg"
+
+print "\nAfter 'require'\n"
+
+for k, v in pairs(_G) do
+    print(k, v)
+end
+
+-- finito
