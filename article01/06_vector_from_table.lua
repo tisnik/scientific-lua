a9ac70eedf6d4c490b52c843f1a5443abf65e3c5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Conversion from table to vector

---
+++ article01/06_vector_from_table.lua
@@ -0,0 +1,21 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 6:
+-- Vytvoření (jednorozměrného) vektoru z tabulky
+--
+
+alg = require "sci.alg"
+
+t = {1, 2, 3, 4, 5}
+
+-- vytvoření jednorozměrného vektoru z tabulky
+vec = alg.tovec(t)
+
+print "Vector value\n"
+print(vec)
+
+-- finito
