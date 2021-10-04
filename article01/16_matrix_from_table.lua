0e7196322a4eeb2f19b9b12bcad5496a700cad3b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Conversion from table when items are of improper type

---
+++ article01/16_matrix_from_table.lua
@@ -0,0 +1,27 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 16:
+-- Pokus o vytvoření matice z tabulky, která obsahuje prvek typu řetězec
+--
+
+alg = require "sci.alg"
+
+-- běžná tabulka jazyka Lua
+tbl = {{1,  2,  3,  4},
+       {5,  6,  7,  8},
+       {9, 10, 11, "foobar"}}
+
+-- konverze tabulky na matici
+mat = alg.tomat(tbl)
+
+-- počet prvků
+print(#mat)
+
+print "Matrix value\n"
+print(mat)
+
+-- finito
