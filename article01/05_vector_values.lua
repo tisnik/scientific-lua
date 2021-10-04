420256aabeb6015291edf8ee66d930eaf2ac5521
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Changing vector item values

---
+++ article01/05_vector_values.lua
@@ -0,0 +1,24 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 5:
+-- Vytvoření (jednorozměrného) vektoru, inicializace prvků
+--
+
+alg = require "sci.alg"
+
+-- alokace vektoru
+vec = alg.vec(10)
+
+-- naplnění prvků vektoru
+for i = 1, 10 do
+    vec[i] = 1/i
+end
+
+print "Vector value\n"
+print(vec)
+
+-- finito
