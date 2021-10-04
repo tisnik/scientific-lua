cf1aaca7e25adacecbf7d4a2688f36719a641931
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Conversion from table to vector with type specification

---
+++ article01/07_vector_type_int8.lua
@@ -0,0 +1,24 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 7:
+-- Specifikace typů prvků vektoru: 8bitová celá čísla
+--
+
+alg = require "sci.alg"
+
+t = {0, 1, 2, 3, 4, 100, 127, 128, 129, 255}
+
+-- konstruktor vektoru daného typu
+int8vec = alg.typeof("int8_t").tovec
+
+-- vytvoření jednorozměrného vektoru z tabulky
+vec = int8vec(t)
+
+print "Vector value\n"
+print(vec)
+
+-- finito
