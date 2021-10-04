fb0fb0e83a7b42d080b61c566b4de7adcff26542
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Conversion from table to vector with type specification

---
+++ article01/09_vector_type_bool.lua
@@ -0,0 +1,24 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 9:
+-- Specifikace typů prvků vektoru: pravdivostní hodnoty
+--
+
+alg = require "sci.alg"
+
+t = {0, 1, 2, 3, 4, 100, 127, 128, 129, 255}
+
+-- konstruktor vektoru daného typu
+boolvec = alg.typeof("bool").tovec
+
+-- vytvoření jednorozměrného vektoru z tabulky
+vec = boolvec(t)
+
+print "Vector value\n"
+print(vec)
+
+-- finito
