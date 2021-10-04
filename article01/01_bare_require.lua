1e69f5578cefbccaa342441b462c814363830f59
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:02:35 +0200

    Bare require of sci.alg

---
+++ article01/01_bare_require.lua
@@ -0,0 +1,13 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Úvodní článek o knihovně SciLua
+--
+-- Demonstrační příklad číslo 1:
+-- Načtení knihovny "sci.alg" bez přiřazení výsledku do proměnné
+--
+
+require "sci.alg"
+
+-- finito
