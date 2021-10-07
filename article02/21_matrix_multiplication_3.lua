3a40c90e8bb60d7da460460658c939460cd0b5d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-07 07:32:04 +0200

    Matrix multiplication, third example

---
+++ article02/21_matrix_multiplication_3.lua
@@ -0,0 +1,38 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Manipulace s vektory a maticemi
+--
+-- Demonstrační příklad číslo 21:
+-- Maticový součin pro nečtvercové matice
+--
+
+-- import knihovny sci.alg
+alg = require "sci.alg"
+
+-- tabulky z nichž se vytvoří matice
+t1 = {{1, 2, 3}, {4, 5, 6}}
+t2 = {{0, -1}, {-1, 0}, {1, 1}}
+
+-- vytvoření dvourozměrných matic z tabulky
+mat1 = alg.tomat(t1)
+mat2 = alg.tomat(t2)
+
+print "Matrix value #1\n"
+print(mat1)
+
+print "\nMatrix value #2\n"
+print(mat2)
+
+-- matice pro uložení výsledku
+result = alg.mat(2, 2)
+
+-- funkce alg.pow
+alg.mul(result, mat1, mat2, false, false)
+
+-- výpis výsledné matice
+print "\nResult matrix\n"
+print(result)
+
+-- finito
