9224c93c6af37a249f81148a919fcb83aa110d15
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-07 07:31:59 +0200

    Added link to article where this example is used

--- article01/01_bare_require.lua
+++ article01/01_bare_require.lua
@@ -3,6 +3,7 @@
 -- https://www.root.cz/serialy/programovaci-jazyk-lua/
 --
 -- Úvodní článek o knihovně SciLua
+-- https://www.root.cz/clanky/predstaveni-projektu-scilua/
 --
 -- Demonstrační příklad číslo 1:
 -- Načtení knihovny "sci.alg" bez přiřazení výsledku do proměnné
