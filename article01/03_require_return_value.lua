8da530f364a5fffb32089c567c180bacafd5ab46
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-07 07:31:59 +0200

    Added link to article where this example is used

--- article01/03_require_return_value.lua
+++ article01/03_require_return_value.lua
@@ -3,6 +3,7 @@
 -- https://www.root.cz/serialy/programovaci-jazyk-lua/
 --
 -- Úvodní článek o knihovně SciLua
+-- https://www.root.cz/clanky/predstaveni-projektu-scilua/
 --
 -- Demonstrační příklad číslo 3:
 -- Načtení knihovny "sci.alg", uložení a zobrazení vrácené hodnoty
