dc5ae3569d56ae6a6538934a4fd458a11a8f70b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-07 07:32:04 +0200

    Matrix pow operation

---
+++ article02/18_matrix_pow.lua
@@ -0,0 +1,33 @@
+--
+-- Seriál Programovací jazyk Lua
+-- https://www.root.cz/serialy/programovaci-jazyk-lua/
+--
+-- Manipulace s vektory a maticemi
+--
+-- Demonstrační příklad číslo 18:
+-- Funkce alg.pow
+--
+
+-- import knihovny sci.alg
+alg = require "sci.alg"
+
+-- tabulky z nichž se vytvoří matice
+t = {{2, -1}, {0, 1}}
+
+-- vytvoření matice z tabulky
+mat = alg.tomat(t)
+
+print "Original matrix value\n"
+print(mat)
+
+-- matice pro uložení výsledku
+result = alg.mat(2, 2)
+
+-- funkce alg.pow
+alg.pow(result, mat, 2)
+
+-- výpis výsledné matice
+print "\nResult matrix\n"
+print(result)
+
+-- finito
